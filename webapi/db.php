5e39970233faa7beffcfb6eca54326411789a0f4
dudi rudiana <dudirudiana1994@gmail.com>
2021-02-18 22:20:33 +0700

    first

---
+++ webapi/db.php
@@ -0,0 +1,21 @@
+<?php
+
+// // DEV
+$server= "localhost";
+$username ="root";
+$password="";
+$dbname ="penjualan";
+
+// LIVE
+// $server= "sql103.epizy.com";
+// $username ="epiz_27806660";
+// $password="";
+// $dbname ="epiz_27806660_penjualan";
+
+$conn = new mysqli($server, $username, $password, $dbname);
+
+
+// if ($conn->connect_error) {
+// 	die("connection failed" . $conn->connect_error );
+// }
+//  echo "Connect successfully done";
